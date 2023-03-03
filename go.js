cd5a47fd80986238882586f9167217880d09c2fe
fabio <fabiofalopes@gmail.com>
2023-03-03 01:25:10 +0000

    yo

---
+++ go.js
@@ -0,0 +1,8 @@
+function showDetails() {
+    document.getElementById("details").style.display = "block";
+  }
+  
+  function hideDetails() {
+    document.getElementById("details").style.display = "none";
+  }
+  
